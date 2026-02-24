4ead8f794dc0e82a390de03dd90848c272e434ea
zeinab-moosavi <zeinab20moosavi@gmail.com>
2026-02-24 10:32:32 +0330

    Inisialized the game

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        screen.fill("black")
+        pygame.display.flip()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
